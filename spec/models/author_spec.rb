7513b24ea0c71312b9344f0a1f6710badc7009fe
NikkelM <nikkel.mollenhauer@student.hpi.de>
2021-11-08 19:06:31 +0100

    Fix create params #5

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe "Author page", type: :model do
     it "should have a first_name, last_name, homepage field" do
-        author = Author.create("Alan", "Turing", "http://wikipedia.de/Alan_Turing")
+        author = Author.create(first_name:"Alan", last_name:"Turing", homepage:"http://wikipedia.de/Alan_Turing")
         expect(author.first_name).to eq("Alan")
         expect(author.last_name).to eq("Turing")
         expect(author.homepage).to eq("http://wikipedia.de/Alan_Turing")
