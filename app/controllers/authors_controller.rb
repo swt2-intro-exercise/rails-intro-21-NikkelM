28cd4266f127e08fa4d3af3a271768dda5282e3b
NikkelM <nikkel.mollenhauer@student.hpi.de>
2021-11-08 19:02:19 +0100

    Failing test #5

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,4 +1,21 @@
 class AuthorsController < ApplicationController
+    def show
+        @authors = Author.find(params[:id])
+    end
+
     def new
     end
+
+    def create
+        @authors = Author.new(author_params)
+
+        @authors.save
+        redirect_to @authors
+    end
+
+
+    private
+        def author_params
+            params.require(:author).permit(:first_name, :last_name, :homepage)
+        end
 end
---
+++ spec/models/author_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe "Author page", type: :model do
+    it "should have a first_name, last_name, homepage field" do
+        author = Author.create("Alan", "Turing", "http://wikipedia.de/Alan_Turing")
+        expect(author.first_name).to eq("Alan")
+        expect(author.last_name).to eq("Turing")
+        expect(author.homepage).to eq("http://wikipedia.de/Alan_Turing")
+    end
+  end
