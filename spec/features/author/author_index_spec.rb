efe2646782c00d62fb544f95de1ba56854c7fdae
NikkelM <57323886+NikkelM@users.noreply.github.com>
2021-11-09 19:33:23 +0100

    Fix test #8

--- spec/features/author/author_index_spec.rb
+++ spec/features/author/author_index_spec.rb
@@ -1,11 +1,15 @@
 require 'rails_helper'
 
 describe "Author index page", type: :feature do
+  subject { page }
+
   it "should exist at 'index' and render without error" do
     visit authors_path
   end
 
   it "should contain a link to the create a new author page" do
-    expect(home_index_path).to have_link 'Author overview', href: authors_path
+    visit authors_path
+    should have_link('New Author', href: new_author_path)
   end
+
 end
