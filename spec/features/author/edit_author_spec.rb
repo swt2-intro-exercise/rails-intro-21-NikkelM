239ec44bb7bf2f4461bd810023fcaa6926216aa9
NikkelM <nikkel.mollenhauer@student.hpi.de>
2021-11-10 20:40:43 +0100

    Add test for proper author edit

--- spec/features/author/edit_author_spec.rb
+++ spec/features/author/edit_author_spec.rb
@@ -5,4 +5,15 @@ describe 'Authors edit page', type: :feature do
 	author = Author.create(first_name:"Alan", last_name:"Turing")
     visit edit_author_path(author)
   end
+
+  it 'properly edits an author' do
+	author = Author.create(first_name:"Alan", last_name:"Turinger")
+    visit edit_author_path(author)
+	fill_in 'author[last_name]', with: "Turing"
+    submit_form
+	author.reload
+	expect(author.last_name).to eq('Turing')
+  end
+
+  
 end
