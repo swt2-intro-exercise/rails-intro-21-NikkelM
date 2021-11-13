a75b66ffc08b0766da02dd04d4266e0bc684d36f
NikkelM <nikkel.mollenhauer@student.hpi.de>
2021-11-13 16:42:38 +0100

    Add tests, features #15

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,7 +1,7 @@
 class Author < ApplicationRecord
-    def name
-        return first_name + " " + last_name
-    end
-    validates :first_name, presence: true
-    validates :last_name, presence: true
+	def name
+		return first_name + " " + last_name
+	end
+	validates :first_name, presence: true
+	validates :last_name, presence: true
 end
---
+++ spec/features/papers/new_paper_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+describe "New Paper page", type: :feature do
+  it "should exist at 'new_author_path' and render without error" do
+    visit new_paper_path
+  end
+  
+  it "should contain input fields for title, venue and year" do
+    visit new_paper_path
+    expect(page).to have_field('paper[title]')
+    expect(page).to have_field('paper[venue]')
+    expect(page).to have_field('paper[year]')
+  end
+
+  it "should not allow a paper to be created without a year" do
+    paper = Paper.create(title:"Funny Paper", venue:"HPI Conference")
+    expect(paper).to_not be_valid
+  end
+
+	it "should not allow a paper to be created with the year not being an integer" do
+    paper = Paper.create(title:"Funny Paper", venue:"HPI Conference", year:"Hello")
+		paper2 = Paper.create(title:"Funny Paper", venue:"HPI Conference", year:"2020.5")
+    expect(paper).to_not be_valid
+  end
+
+	it "should not allow a paper to be created without a venue" do
+    paper = Paper.create(title:"Funny Paper", year:"2020")
+    expect(paper).to_not be_valid
+  end
+
+	it "should not allow a paper to be created without a title" do
+    paper = Paper.create(venue:"HPI Conference", year:"2020")
+    expect(paper).to_not be_valid
+  end
+
+  it "should show the user an error when the entered paper is invalid" do
+    visit new_paper_path
+    fill_in 'paper[title]', with: "Funny Name"
+    fill_in 'paper[venue]', with: "HPI Conference"
+    submit_form
+    expect(page).to have_text('error')
+  end
+
+end
